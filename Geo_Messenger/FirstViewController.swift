ee1220a1a4799c1832c3405f2b73754357a3ac2e
Tommy Mota <thomas.mota@marquette.edu>
2018-09-17 22:43:33 -0500

    Initial Commit

---
+++ Geo_Messenger/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Geo_Messenger
+//
+//  Created by Tommy  Mota on 9/17/18.
+//  Copyright © 2018 Mota. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
